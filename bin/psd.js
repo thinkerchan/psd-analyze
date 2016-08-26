328cdc3e971bf927a4fc4e77211257e964104417
Thinker Chan <291810213@qq.com>
2016-08-26 17:04:30 +0800

    自动保存图层

--- bin/psd.js
+++ bin/psd.js
@@ -1,35 +1,39 @@
 #!/usr/bin/env node
 var fs = require("fs"),
     path = process.cwd();
-
 var argv = require('yargs')
   .alias('n', 'name')
   .argv;
-
 var exec = require('child_process').exec;
-
 var fs=require('fs'),
     PSD = require('psd'),
     psdName = argv.n;
-
 var psd = PSD.fromFile("psd/"+psdName+".psd");
     psd.parse();
-
 var data = psd.tree().export();
-var css = '', html='', imgSrc='images/';
+var css = '',
+    html='',
+    imgSrc='images/',
+    prefix = '',
+    suffix = '';
 for (var i = data.children.length - 1; i >= 0; i--) {
   var el = data.children[i] ;
   css += '.'+el.name+'{position:absolute;'+'width:'+el.width+'px; '+'height:'+el.height+'px; '+'top:'+el.top+'px; '+'left:'+el.left+'px; '+'z-index:0; }'+'\n';
-  html+='<div class='+'"'+el.name+'"'+'><img src='+'"'+imgSrc+el.name+'@2x.png"'+'></div>'+'\n';
+  html+='<div class='+'"'+el.name+'"'+'><img src='+'"'+prefix+imgSrc+el.name+suffix+'.png"'+'></div>'+'\n';
 }
-
-
 exec('cd psd && mkdir '+psdName,
   function (error, stdout, stderr) {
     PSD.open("psd/"+psdName+".psd").then(function (psd) {
-      // css
+      fs.mkdir("psd/"+psdName+'/images',function(){
+      });
+      psd.tree().descendants().reverse().forEach(function (layer,index) {
+        var img = layer.layer.image;
+        var name  = layer.layer.legacyName;
+          img.saveAsPng('psd/'+psdName+'/images/'+name+".png").catch(function (err) {
+              console.log(err.stack);
+          });
+      });
       fs.writeFileSync('psd/'+psdName+'/'+psdName+'.css',css);
-      // html
       fs.writeFileSync('psd/'+psdName+'/'+psdName+'.html',html);
       console.log(psdName+".psd Finished!");
     });
